9a96d769020fa69be57910ea7afa1810b2046096
ASN-EAAA <asnielsen789@gmail.com>
2023-11-30 11:04:39 +0100

    bidservice

---
+++ BidServiceTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BidServiceTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
