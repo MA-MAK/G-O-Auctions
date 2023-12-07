46001ff558fe0d151a235638ef4a1b4319949f4d
KarolineQvick <103989440+KarolineQvick@users.noreply.github.com>
2023-12-07 09:53:41 +0100

    get customer virker

--- CustomerService/Models/Customer.cs
+++ CustomerService/Models/Customer.cs
@@ -9,6 +9,7 @@ public class Customer
     public string Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    public bool Premium { get; set; }
 
     // Constructor
     /*
