19fdf90f798cefadf08e2cd4d3ed8f2a8c296eb3
KarolineQvick <103989440+KarolineQvick@users.noreply.github.com>
2023-12-15 16:40:46 +0100

    vault

--- AuctionService/Program.cs
+++ AuctionService/Program.cs
@@ -6,6 +6,15 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using AuctionService.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using System.Text;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.Commons;
 
 //var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
 //logger.Debug("init main");
@@ -22,33 +31,103 @@ try
     builder.Logging.ClearProviders();
     builder.Host.UseNLog();
     builder.Services.AddLogging();
-    builder.Services.AddHttpClient("ItemService", client =>
-    {
-        client.BaseAddress = new Uri("http://localhost:5164");
-    });
+    builder.Services.AddHttpClient(
+        "ItemService",
+        client =>
+        {
+            client.BaseAddress = new Uri("http://localhost:5164");
+        }
+    );
 
-    builder.Services.AddHttpClient("BidService", client =>
-    {
-        client.BaseAddress = new Uri("http://localhost:5223");
-    });
+    builder.Services.AddHttpClient(
+        "BidService",
+        client =>
+        {
+            client.BaseAddress = new Uri("http://localhost:5223");
+        }
+    );
 
     builder.Services.AddSingleton<IAuctionRepository, AuctionRepository>();
 
     builder.Services.AddSingleton<IItemRepository, ItemRepository>(
-        b => new ItemRepository(b.GetService<IHttpClientFactory>()
-        .CreateClient("ItemService"), 
-        builder.Services.BuildServiceProvider().GetRequiredService<ILogger<ItemRepository>>()));
-    
+        b =>
+            new ItemRepository(
+                b.GetService<IHttpClientFactory>().CreateClient("ItemService"),
+                builder.Services
+                    .BuildServiceProvider()
+                    .GetRequiredService<ILogger<ItemRepository>>()
+            )
+    );
+
     builder.Services.AddSingleton<IBidRepository, BidRepository>(
-        b => new BidRepository(b.GetService<IHttpClientFactory>()
-        .CreateClient("BidService"), 
-        builder.Services.BuildServiceProvider().GetRequiredService<ILogger<BidRepository>>()));
+        b =>
+            new BidRepository(
+                b.GetService<IHttpClientFactory>().CreateClient("BidService"),
+                builder.Services.BuildServiceProvider().GetRequiredService<ILogger<BidRepository>>()
+            )
+    );
 
     // MongoDB configuration
     var connectionString = builder.Configuration.GetConnectionString("MongoDBConnection");
     var databaseName = builder.Configuration.GetSection("MongoDBSettings:DatabaseName").Value;
-    var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<MongoDBContext>>();
-    builder.Services.AddSingleton<MongoDBContext>(provider => new MongoDBContext(logger, builder.Configuration));
+    var logger = builder.Services
+        .BuildServiceProvider()
+        .GetRequiredService<ILogger<MongoDBContext>>();
+    builder.Services.AddSingleton<MongoDBContext>(
+        provider => new MongoDBContext(logger, builder.Configuration)
+    );
+
+    // Vault
+    var EndPoint = Environment.GetEnvironmentVariable("vault") ?? "https://localhost:8201/";
+    var httpClientHandler = new HttpClientHandler();
+    httpClientHandler.ServerCertificateCustomValidationCallback = (
+        message,
+        cert,
+        chain,
+        sslPolicyErrors
+    ) =>
+    {
+        return true;
+    };
+
+    // Initialize one of the several auth methods.
+    IAuthMethodInfo authMethod = new TokenAuthMethodInfo("00000000-0000-0000-0000-000000000000");
+    // Initialize settings. You can also set proxies, custom delegates etc. here.
+    var vaultClientSettings = new VaultClientSettings(EndPoint, authMethod)
+    {
+        Namespace = "",
+        MyHttpClientProviderFunc = handler =>
+            new HttpClient(httpClientHandler) { BaseAddress = new Uri(EndPoint) }
+    };
+    IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+    // Use client to read a key-value secret.
+    Secret<SecretData> kv2Secret = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(
+        path: "auctionSecrets",
+        mountPoint: "secret"
+    );
+    string mySecret = kv2Secret.Data.Data["Secret"].ToString();
+    // Console.WriteLine($"MinKode: {minkode}");
+    string myIssuer = Environment.GetEnvironmentVariable("Issuer") ?? "http://localhost:5050";
+    var _logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+    _logger.LogInformation($"### myIssuer {myIssuer}");
+    _logger.LogInformation($"### mySecret {mySecret}");
+
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = myIssuer,
+                ValidAudience = "http://localhost",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
+            };
+        });
 
     var app = builder.Build();
 
@@ -62,11 +141,11 @@ try
     app.UseHttpsRedirection();
 
     app.UseAuthorization();
+    app.UseAuthentication();
 
     app.MapControllers();
 
     app.Run();
-
 }
 catch (Exception ex)
 {
